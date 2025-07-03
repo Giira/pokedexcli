904250d37174a7197d69628dc913aa965dc4b01a
Giira <euan.a.marsh@gmail.com>
2025-07-03 12:20:59 +0100

    Cache setup

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,46 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	mu       sync.Mutex
+	entry    map[string]cacheEntry
+	interval time.Duration
+}
+
+func NewCache(interval time.Duration) Cache {
+	return Cache{
+		interval: interval,
+	}
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.entry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	data, ok := c.entry[key]
+	if !ok {
+		return nil, false
+	}
+	return data.val, true
+}
+
+func (c *Cache) reapLoop() {
+	continue
+}
